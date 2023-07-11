fb856692f2a0ab27886bf7fd901e98f0cf67f0bc
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-07-11 05:12:13 -0300

    style: Category buttons styling

---
+++ src/components/Button/style.ts
@@ -0,0 +1,44 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+`;
+
+export const ContainerButtonBasic = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 714px;
+  height: 146.67px;
+  background-color: #FF9800;
+  border-radius: 100px;
+  margin-bottom: 47px;
+`;
+
+export const ContainerButtonIntermediary = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 714px;
+  height: 146.67px;
+  background-color: #2196F3;
+  border-radius: 100px;
+  margin-bottom: 47px;
+`;
+
+export const ContainerButtonAdvanced = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 714px;
+  height: 146.67px;
+  background-color: #FF1744;
+  border-radius: 100px;
+`;
