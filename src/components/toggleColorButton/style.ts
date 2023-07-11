051cbc6b3ba13b4b0b198c4e4fd32f5bcd3b36e0
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-07-11 05:09:46 -0300

    style: toggleColorButton styling

---
+++ src/components/toggleColorButton/style.ts
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+export const ContainerButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  width: 102px;
+  height: 46px;
+  background-color: white;
+  border-radius: 30px;
+  margin-top: 38px;
+  margin-bottom: 41px;
+  padding: 4px;
+`;
+
+export const Button = styled.button`
+  width: 38px;
+  height: 38px;
+  background-color: black;
+  border-radius: 30px;
+`;
