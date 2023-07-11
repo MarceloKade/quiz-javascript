cd2f9a690062ca37c36e3e0e54b3dd0c45ea0669
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-07-11 05:07:32 -0300

    Created buttons to change the background color of the page.

---
+++ src/components/toggleColorButton/ToggleColorButton.tsx
@@ -0,0 +1,13 @@
+'use client'
+import * as B from "./style";
+
+
+export default function ToggleColorButton() {
+    return (
+        <>
+            <B.ContainerButton>
+                <B.Button />
+            </B.ContainerButton>
+        </>
+    );
+}
