f8c1fc2774dbdc7b03d8698f8b1129f3bed76c07
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-07-12 10:07:16 -0300

    Created text component

---
+++ src/components/text/Text.tsx
@@ -0,0 +1,11 @@
+'use client'
+import * as T from "../text/style";
+export default function Text() {
+    return (
+        <>
+            <T.TextQuiz>Quiz <T.TextJavaScript>JavaScript</T.TextJavaScript></T.TextQuiz>
+            <T.TextCategory>Escolha o seu nível de dificuldade</T.TextCategory>
+
+        </>
+    );
+}
