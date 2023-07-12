873dadbc2dd8a44606012a0aee90fbed09bb059c
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-07-12 10:08:15 -0300

    Text component styling

---
+++ src/components/text/style.ts
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+export const Text = styled.p`
+  font-size: 80px;
+  color: white;
+`;
+
+export const TextQuiz = styled.p`
+  font-size: 80px;
+  color: #FF9800;
+`;
+
+export const TextJavaScript = styled.span`
+  font-size: 80px;
+  color: #FF1744;
+`;
+
+export const TextCategory = styled.p`
+  font-size: 80px;
+  color: #2196F3;
+  margin-bottom: 41px;
+`;
