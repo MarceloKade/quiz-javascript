381202b133d1fe55184c6bb47bb86038b941284a
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-07-23 19:40:01 -0300

    fix: Revised the import for Container as there was an error during the deployment build.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import ContainerPage from "@/components/button/ContainerPage";
+'use client'
+import { ButtonCategory } from '@/components/button/Button';
+import { ContainerButton } from '@/components/button/style';
+import Text from '@/components/text/Text';
 
 export default function Home() {
   return (
     <>
       <main>
-        <ContainerPage />
+        <ContainerButton>
+          <Text />
+          <ButtonCategory text="Básico" category="basic" type={'basic'} />
+          <ButtonCategory text="Intermediário" category="intermediary" type={'intermediary'} />
+          <ButtonCategory text="Avançado" category="advanced" type={'advanced'} />
+        </ContainerButton>
       </main>
     </>
   );
